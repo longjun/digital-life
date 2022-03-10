8e1aed143428aa815e5acb880435f18120e31d20
Joe.Wang <joe@rethk.com>
2022-03-10 15:54:40 +0800

    fix bug

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("github_token", help="github_token")
     parser.add_argument("repo_name", help="repo_name")
-    parser.add_argument("--issue_num", help="issue_number", default=None, required=True)
+    parser.add_argument("issue_num", help="issue_number")
     parser.add_argument("--message", help="comment_message", default=None, required=False)
     options = parser.parse_args()
     main(options.github_token, options.repo_name, options.issue_num, options.message)
